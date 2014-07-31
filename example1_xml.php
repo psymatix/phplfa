c57e8a3dd075e841e107059c1590305f94f621f7
Simon Agamah <psymatix@gmail.com>
2014-07-31 16:09:05 +0100

    New network added about to debug functions to solve 6 bus

---
+++ example1_xml.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once("Math/Matrix.php");
+require_once("Math/Complex.php");
+require_once("Math/ComplexOp.php");
+
+//Autoloader
+
+function PSAutoloader($class){
+    include 'classes/' .  $class . '.php';
+}
+
+spl_autoload_register('PSAutoloader');
+//initialization procedures
+
+$time = -microtime(true);
+$mem = -memory_get_usage();
+
+$p = new PowerNetwork();
+
+$p->loadXMLNetwork("xmlnetworks/example1.xml");
+
+
+$p->initializeNetwork();
+
+lfNR::solve($p, 2.5E-4, 12);
+printNetwork($p); 
+
+$time += microtime(true);
+$mem += memory_get_usage();
+$memkb = $mem/1024;
+
+echo "<br><hr>";
+echo "completed in $time seconds";
+echo "<br>";
+echo "total memory allocated: $memkb";
+
+function printNetwork($n){
+    
+        
+     foreach($n->buses as $bus){ 
+     
+                
+                  echo "Bus Number:" .  $bus->number . "<br>";
+                  print_r("V:" . $bus->voltagePU . "<br>"); 
+                  print_r("D:" . $bus->voltageAngle . "<br>"); 
+                  if($bus->S){
+                  print_r("P:" . $bus->S->getReal() . "<br>"); 
+                  print_r("Q:" . $bus->S->getIm() . "<br>"); 
+                  }
+                  echo "<br><hr>";
+                 
+                  
+
+  
+ }
+ 
+ echo "converged after " . lfNR::$step . " steps <br><hr>";
+ print_r($n->voltageControlledBuses);
+ 
+}
+
+?>
